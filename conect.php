c474a944869151d4a013291b4f983b96a3537e34
Caroline da Silva Rodrigues <loracsilva@gmail.com>
2018-03-25 23:47:17 -0300

    Update conect.php

--- conect.php
+++ conect.php
@@ -1,16 +1,16 @@
 <?php
 error_reporting(E_ALL ^ E_DEPRECATED);
     //configurações de banco
-    $host = "teste_diversos.mysql.dbaas.com.br:3306";
-    $usuario = "teste_diversos";
-    $senha = "teste123@";
-    $banco = "teste_diversos";
+    $host = "#:3306";
+    $usuario = "#";
+    $senha = "#";
+    $banco = "#";
     
     //configurações de tabelas
-    $tabela_telefone = "whats";
-    $nome_coluna_telefone = "numero";
-    $nome_coluna_cliente = "user_cli";
-    $nome_cliente = "User Teste";
+    $tabela_telefone = "#";
+    $nome_coluna_telefone = "#";
+    $nome_coluna_cliente = "#";
+    $nome_cliente = "#";
     
     //conexão
     $conn = mysql_connect($host, $usuario, $senha)or die('Erro: ' . mysql_error());; 
